45be22ee2116896bede9ab8c170dc8a4760f51cf
David Lejeune <Lejeune.David@outlook.com>
2017-09-28 08:55:55 +0200

    counting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
+  # better way :
+  match ':controller(/:action(/:id))', :via => :get
+
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
